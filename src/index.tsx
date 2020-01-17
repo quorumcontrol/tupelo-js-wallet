380738df074d1f942b624a4542470734a9fe75ab
Wes Morgan <wesmorgan@icloud.com>
2020-01-17 08:56:02 -0700

    Redirect to HTTPS in JS
    The original Cloudflare redirect seems not to work with IPFS

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+if (window.location.protocol !== 'https:') {
+    location.href = location.href.replace('http://', 'https://');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
