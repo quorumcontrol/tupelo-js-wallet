a788d7a80693a91bd3cc1e66b5eacbd3e102bb91
Wes Morgan <wesmorgan@icloud.com>
2020-02-05 12:34:20 -0700

    Add IPFS node service worker

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const loc = window.location;
-if (loc.protocol !== 'https:') {
+if (loc.hostname !== 'localhost' && loc.protocol !== 'https:') {
     window.location.href = loc.href.replace('http://', 'https://');
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/ipfs-sw.js`)
+} else {
+    console.log('Cannot register service worker')
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -14,4 +20,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
