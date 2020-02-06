008aa130b4dd6ea66c25c4037d15dc88904f6af9
Wes Morgan <wesmorgan@icloud.com>
2020-02-06 14:33:06 -0700

    Stop unregistering service workers

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const loc = window.location;
 if (loc.hostname !== 'localhost' && loc.protocol !== 'https:') {
@@ -16,8 +15,3 @@ if ('serviceWorker' in navigator) {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
