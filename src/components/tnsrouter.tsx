ff674b3f710b2be56f1f5d5399739e8a3bd8d8ee
Wes Morgan <wesmorgan@icloud.com>
2020-02-26 12:06:14 -0700

    Support configured WALLET_DOMAIN and subdomains

--- src/components/tnsrouter.tsx
+++ src/components/tnsrouter.tsx
@@ -7,31 +7,60 @@ import { tnsPath } from './tns';
 export function TNSRouter() {
     const [state, setState] = useState({
         username: "",
+        chaintreeDataPath: "",
         tree: undefined as ChainTree | undefined,
         target: "",
     })
 
+    const walletHostname = process.env.REACT_APP_WALLET_DOMAIN || ""
+    console.log(`walletHostname: ${walletHostname}`)
+    const walletHostnameComponents = walletHostname.split('.').reverse()
+
     useEffect(() => {
         const getTreeForUsername = async () => {
+            // return early if we've already looked up our target
+            if (state.target !== "") {
+                return
+            }
+
             // we reverse the hostname components b/c the hierarchy is easier to work with that way
             const hostnameComponents = window.location.hostname.split('.').reverse()
 
+            console.log(`hostnameComponents: ${hostnameComponents}`)
+
             let username: string
-            if (hostnameComponents[0] === 'me' && hostnameComponents[1] === 'tupelo') {
-                username = hostnameComponents[2] // skip 'me' & 'tupelo'
+            let chaintreeDataPath: string
+            let allMatch = true
+            walletHostnameComponents.forEach((c, i) => {
+                if (hostnameComponents[i] !== c) {
+                    allMatch = false
+                }
+            })
+            if (allMatch) {
+                // skip wallet hostname components
+                username = hostnameComponents[walletHostnameComponents.length]
+                chaintreeDataPath = hostnameComponents.slice(walletHostnameComponents.length + 1).reverse().join('/')
             } else {
                 // TODO: Also support looking up a _tnslink TXT record for this?
-                username = window.location.hostname
+                // Assumes first two components are username (i.e. TLD and component just to the left of that)
+                // Everything else should be TNS pointers inside that user's chaintree.
+                username = hostnameComponents.slice(0, 2).reverse().join('.')
+                chaintreeDataPath = hostnameComponents.slice(3).reverse().join('/')
             }
 
             let userTree: ChainTree
             try {
+                console.log(`getting tree for username ${username}`)
                 userTree = await getUserTree(username)
                 setState((s) => {
-                    return { ...s, username: username, tree: userTree }
+                    return {
+                        ...s,
+                        username: username,
+                        chaintreeDataPath: chaintreeDataPath,
+                        tree: userTree
+                    }
                 })
             } catch(e) {
-                console.log("no user tree") // TODO: delete this line once working
                 // just ignore
                 return
             }
@@ -40,9 +69,12 @@ export function TNSRouter() {
     })
 
     useEffect(() => {
-        const getIPFSAddr = async (tree: ChainTree) => {
-            // TODO: Add support for nested names & pointers
-            const pointer = await tree.resolveData(`${tnsPath}/@`)
+        const getIPFSAddr = async (tree: ChainTree, dataPath: string) => {
+            if (dataPath === "") {
+                dataPath = '@'
+            }
+
+            const pointer = await tree.resolveData(`${tnsPath}/${dataPath}`)
 
             const target = `https://ipfs.io${pointer.value}`
 
@@ -53,9 +85,9 @@ export function TNSRouter() {
         }
 
         if (state.tree !== undefined && state.target === "") {
-            getIPFSAddr(state.tree)
+            getIPFSAddr(state.tree, state.chaintreeDataPath)
         }
-    }, [state.tree])
+    }, [state.tree, state.chaintreeDataPath])
 
     useEffect(() => {
         const redirectToTarget = () => {
@@ -70,6 +102,9 @@ export function TNSRouter() {
     }, [state.target])
 
     return (
-        <p>Redirecting to {state.target === "" ? <Loader/> : state.target}</p>
+        <div>
+            <h3>Welcome to the {walletHostname} Tupelo Naming Service gateway.</h3>
+            <p>Redirecting to {state.target === "" ? <span>... <Loader/></span> : state.target}</p>
+        </div>
     )
 }
